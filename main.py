ccbc5d6c052a863e5e973b6efc74fa0e0154732d
unknown <you@example.com>
2023-02-06 10:28:34 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+num1=550
+num2=450
+result=num1+num2
+print(result)
+
+num3=50
+num4=100
+result=num3*num4
+print(result)
+
+
---
+++ today.py
@@ -0,0 +1,7 @@
+school="Emobilis"
+print(school)
+town="parklands"
+print(town)
+x="Hello"
+y="World"
+print(x+y)
