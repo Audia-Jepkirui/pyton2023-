61608860b0b63f8cbd0aa082a13807d149724c79
unknown <you@example.com>
2023-02-09 09:16:32 +0300

    data structures and if statements

---
+++ Conditions/If statements.py
@@ -0,0 +1,15 @@
+dial=int(input("Enter a number"))
+if dial==1:
+    print("Enrol for bonga points")
+elif dial==2:
+    print("Lipa mdogomdogo")
+elif dial==3:
+    print("No expiry")
+elif dial==4:
+    print("Data deals")
+elif dial==5:
+    print("Video bundles")
+elif dial==6:
+    print("daily bundles")
+else:
+    print("MORE")
---
+++ Data Structures/Dictionaries.py
@@ -0,0 +1,7 @@
+#name="Audia"
+#print(name)
+#Key and value pairs
+#arranged in order
+friends={"loui": 25, "lissa":22, "rita":29}
+friends["marie"]=23
+print(friends)
